219cc386c540e5cabfb448b295f2ef6e79fed7b9
je-lwc <79720377+je-lwc@users.noreply.github.com>
2022-04-18 15:55:01 -0400

    'save'

--- src/lwc/accountTeamCommissionListRow/accountTeamCommissionListRow.js
+++ src/lwc/accountTeamCommissionListRow/accountTeamCommissionListRow.js
@@ -1,6 +1,24 @@
 import { LightningElement, api } from 'lwc';
 
 export default class AccountTeamCommissionListRow extends LightningElement {
+  _member = {};
   @api
-  member = {};
+  set member(v) {
+    this._member = v;
+  }
+  get member() {
+    return this._member;
+  }
+
+  handlePercentageUpdate(value) {
+    this._member = { ...this.member, pct: value };
+  }
+
+  handleSliderInput({ detail: value }) {
+    this.handlePercentageUpdate(value);
+  }
+
+  handleNumberInput({ currentTarget: { value } }) {
+    this.handlePercentageUpdate(value);
+  }
 }
